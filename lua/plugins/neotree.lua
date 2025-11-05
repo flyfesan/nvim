be8817e8ab85ae5e782365b2febcc5f4c19c7ea9
zamaht <hamza.nanouche@gmail.com>
2025-11-05 14:38:02 +0100

    Added autoclose plugin and disabled transparency on nvim

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,14 +1,24 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
-  },
-  lazy = false,
-  config = function()
-   vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle<CR>', { desc = 'Show tree file system' })
-    vim.keymap.set('n', '<leader>b', ':Neotree focus<CR>', { desc = 'focus on file tree' })
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = false,
+	config = function()
+		require("neo-tree").setup({
+			enable_git_status = true,
+			enable_diagnostics = true,
+			options = {
+				theme = "catppuccin",
+			},
+		})
+
+		vim.keymap.set("n", "<C-b>", ":Neotree filesystem toggle<CR>", { desc = "Show tree file system" })
+		vim.keymap.set("n", "<leader>b", ":Neotree focus<CR>", { desc = "focus on file tree" })
+		vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+	end,
 }
--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,5 +1,6 @@
 return {
 	"xiyaowong/transparent.nvim",
+	enabled = false,
 	config = function()
 		require("transparent").clear_prefix("NeoTree")
 		require("transparent").clear_prefix("lualine")
