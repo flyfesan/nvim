3b1c7ff54fa03dd724b22ff287f343a488be3f36
flyfesan <hamza.nanouche@proton.me>
2026-01-22 15:08:16 +0100

    fix line numbers not showing

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -6,7 +6,7 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.relativenumber = false
+vim.opt.number = true
 vim.mouse = "nvi"
 
 vim.keymap.set("", "<C-a>", ":wqa<CR>", { desc = "save all files in neovim and quit" })
