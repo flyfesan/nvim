7dd7fc38feafba9c4356c8b504ab8c4637db6c0a
flyfesan <hamza.nanouche@proton.me>
2026-02-07 18:19:35 +0100

    updated packages and changed lualine to include parent directory of the current file

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,6 +5,14 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "catppuccin",
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { { "filename", path = 4 }, "buffers" },
+					lualine_x = { "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
 			},
 		})
 	end,
