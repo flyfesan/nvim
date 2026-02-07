4ed5c03692e34a01f074ef6116c6d6405e32a32c
flyfesan <hamza.nanouche@proton.me>
2026-02-07 10:01:14 +0100

    fix the live-grep plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,12 @@ return {
 		tag = "0.1.8",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
 		},
 		config = function()
 			local telescope_builtin = require("telescope.builtin")
@@ -16,7 +22,8 @@ return {
 				end
 			end, { desc = "Find files with Telescope" })
 			vim.keymap.set("n", "<leader>fg", function()
-				telescope_builtin.live_grep({ search_dirs = { "%:p" } })
+				-- https://github.com/nvim-telescope/telescope-live-grep-args.nvim
+				telescope_builtin.live_grep()
 			end, { desc = "Live grep with Telescope" })
 			vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, { desc = "Find in buffers with Telescope" })
 			vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags, { desc = "Show help tags" })
@@ -52,7 +59,10 @@ return {
 			})
 			-- To get ui-select loaded and working with telescope, you need to call
 			-- load_extension, somewhere after setup function:
-			require("telescope").load_extension("ui-select")
+			local telescope = require("telescope")
+
+			telescope.load_extension("ui-select")
+			telescope.load_extension("live_grep_args")
 		end,
 	},
 }
